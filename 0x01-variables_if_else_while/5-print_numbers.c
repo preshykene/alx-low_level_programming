518b410a205d0efc0ec8d3b90645d4bb3e1be7b4
preshykene <preciousekene51@gmail.com>
2022-09-09 12:25:47 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: always Return 0
+ *
+ */
+int main(void)
+{
+	int i = 10;
+
+	for (; i < 10; a++)
+		printf("%d\n", i);
+	return (0);
+}
