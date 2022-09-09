3e60651168280ef155b5e1e3c3e5e9dce8293fa1
preshykene <preciousekene51@gmail.com>
2022-09-09 13:31:18 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+0 * Return: always Return 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+		putchar('0' + i);
+	putchar("\n");
+	return (0);
+}
