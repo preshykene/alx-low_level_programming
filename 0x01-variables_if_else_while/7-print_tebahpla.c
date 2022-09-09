eccd2074a289565360e3985a922bb83352616971
preshykene <preciousekene51@gmail.com>
2022-09-09 13:50:25 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always Return 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar (a);
+	putchar('\n');
+	return (0);
+}
