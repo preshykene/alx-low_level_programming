6e6e0249167efb94321d6157b25dab1ba3c790f2
preshykene <preciousekene51@gmail.com>
2022-09-13 12:49:29 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(void)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		}else
+		{
+			for (i = n; i >= 98; i++)
+			{
+				printf("%d", i);
+				if (i != 98)
+				{
+					printf(", ");
+				}
+			}
+		}
+	}
+	printf("\n");
+}
