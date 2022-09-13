6cfefb4f1cdbf296d955f87934a605adad1fc58b
preshykene <preciousekene51@gmail.com>
2022-09-13 09:31:53 -0700

    Script to check if c is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks if c is lower then returns a value 
+ *
+ *@void: Reurns nothing
+ */
+
+int _islower(void)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
