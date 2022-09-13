66c1eb3a1e7ccb98daa34639ae1d98dbe81e9dbc
preshykene <preciousekene51@gmail.com>
2022-09-13 04:11:25 -0700

    Updated readme file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints characters
+ * @c: character to print out
+ * Return: on success 1
+ * 	on error, returns -1 and errno is set approprately
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
