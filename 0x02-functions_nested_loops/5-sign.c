873ff74ded44f247c53e3c7d705e8cca9537a476
preshykene <preciousekene51@gmail.com>
2022-09-13 09:58:20 -0700

    Script to check if n is greater than, less than

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ *
+ *@n: integer assigned to n
+ *
+ * Return: 1 or 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+	return (-1);
+}
