722bdabafd5ee4eec749b27f678a058a2b375d24
preshykene <preciousekene51@gmail.com>
2022-09-13 05:13:54 -0700

    Script to print a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphaphabet - prints alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar("\n");
+}
