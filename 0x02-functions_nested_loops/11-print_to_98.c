69efaca5f977a6feddc14feace3d007377e2ec70
preshykene <preciousekene51@gmail.com>
2022-09-13 13:02:48 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,30 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * print_to_98 - main entry
+ *
+ * @n: input
+ *
+ */
 
-void print_to_98(void)
+void print_to_98(int n)
 {
-	int i;
-
+	int count;
+	
 	if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-		printf("%d", i);
-		if (i != 98)
-		{
-			printf(", ");
-		}
-		}else
-		{
-			for (i = n; i >= 98; i++)
-			{
-				printf("%d", i);
-				if (i != 98)
-				{
-					printf(", ");
-				}
-			}
-		}
-	}
-	printf("\n");
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
