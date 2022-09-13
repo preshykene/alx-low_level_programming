b8f2ee4c24601c523c7cc6c0cfa566ded3fdb677
preshykene <preciousekene51@gmail.com>
2022-09-13 09:07:55 -0700

    Script to print alphabets_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,16 @@
  *@void: Reurns nothing
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
 	}
 	_putchar("\n");
+	}
 }
