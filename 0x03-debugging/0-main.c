7ebfaf5003242b0a5a8d1fc125d151a7b57b4d57
preshykene <preciousekene51@gmail.com>
2022-09-14 07:47:52 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: set to 0
  */
 
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * positiive_or_negative - Entry point
+ * 
+ * @i: print value of i status; zero,
+ *
+ * Return: set to 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
