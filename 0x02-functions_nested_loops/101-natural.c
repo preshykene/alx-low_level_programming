4ab46b4877fab35926e9766ccff3caadbfb41e82
preshykene <preciousekene51@gmail.com>
2022-09-13 17:43:42 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: set to 0
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	for(a = 0; a <= 1024; a++)
+	{
+		if (a % 3 || a % 5)
+		{
+			b += a;
+		}
+
+	}
+	printf("%d", b);
+	return (0);
+}
