6b42586d11bf7b39a7b5a8f8bb9be12cd6dd84c8
preshykene <preciousekene51@gmail.com>
2022-09-14 00:56:43 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	        int i;
+		printf("Infinite loop in<F11><F11>coming :(\n");
+		
+		i = 0;
+
+		/*while (i < 10)*/
+		/*{*/
+		/*	putchar(i);*/
+		/*}*/
+		
+		printf("Infinite loop avoided! \\o/\n");
+		
+		return (0);
+}
