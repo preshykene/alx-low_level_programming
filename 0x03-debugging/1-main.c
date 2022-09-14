3fdb31cc9513f55621c221fb6bc4a9ddcd6020d7
preshykene <preciousekene51@gmail.com>
2022-09-14 03:34:50 -0700

    Updated multiple mains

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-	        int i;
-		printf("Infinite loop in<F11><F11>coming :(\n");
-		
+	int i;
+
+		printf("Infinite loop incoming :(\n");
+
 		i = 0;
 
 		/*while (i < 10)*/
 		/*{*/
 		/*	putchar(i);*/
 		/*}*/
-		
+
 		printf("Infinite loop avoided! \\o/\n");
-		
+
 		return (0);
 }
