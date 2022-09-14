3425d459d60ee26fdc5500ee482a44979b663a77
preshykene <preciousekene51@gmail.com>
2022-09-14 10:32:36 -0700

    Updated version hope it works

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	int count = 0;
 	long int a = 1;
 	long int b = a + 1;
-	long int c = 1;
+	long int c = a + b;
 
 	while (b < 4000000)
 	{
 		a = b;
 		b = c;
 		c = a + b;
-		if ((c < 4000000) && (c % 2 == 0))
+		if ((c < 4000000) && (c % 4 == 0))
 		{
 			count += c;
 		}
