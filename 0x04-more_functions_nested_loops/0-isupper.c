798c3cd447c60cf8ac2a13d4cb175fc2352d1084
preshykene <preciousekene51@gmail.com>
2022-09-15 08:03:28 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks if c is uppercase
+ *
+ * @c: assigned integer to check c
+ *Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c <= 'A')
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for more nested loop and function
