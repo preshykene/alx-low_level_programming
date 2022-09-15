3ad16b317f4c727abc95fe0dffb7f0460a5eda45
preshykene <preciousekene51@gmail.com>
2022-09-15 15:51:49 -0700

    Done with 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 
 	for (height = 1; height <= size; height++)
 	{
-		for (width = 1; width <= size; width++)
+		for (width = 1; width <= (height - size); width++)
 
 			_putchar(' ');
 		for (draw = 1; draw <= size; draw++)
