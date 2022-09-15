81273f51d6b4c949418b39773a83a565d0394463
preshykene <preciousekene51@gmail.com>
2022-09-15 15:47:50 -0700

    Done with 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints triangle with #'s
+ * @size: size of triangles to draw
+ */
+
+void print_triangle(int size)
+{
+	int height, width, draw;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= size; width++)
+
+			_putchar(' ');
+		for (draw = 1; draw <= size; draw++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
