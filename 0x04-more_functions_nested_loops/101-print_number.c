ef6711507ccc9b37bf2512f259c8960023e2c3f1
preshykene <preciousekene51@gmail.com>
2022-09-15 23:32:14 -0700

    Done with 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer without the use of pointers or arrays
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digits, positive = n;
+	double Tang = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+		while (Tang <= positive)
+			Tang *= 10;
+		tens = Tang / 10;
+
+		while (tens >= 1)
+		{
+			digits = positive / tens;
+			_putchar(digits + '0');
+			positive = (positive - (tens * digits))
+				tens / 10;
+		}
+	}
+}
