6c581875f4650478595e9c2c23e73e4b402ef9ad
preshykene <preciousekene51@gmail.com>
2022-09-16 00:09:49 -0700

    Done with 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,20 @@
 
 void print_number(int n)
 {
-	unsigned int tens, digits, positive = n;
-	double Tang = 1;
+	unsigned int positive = n;
 
-	if (n == 0)
+	if (n < 0)
 		_putchar('0');
-	else
-	{
+		positive = -positive;
+
 		if (n < 0)
 		{
 			positive = n * -1;
 			_putchar('-');
 		}
-		while (Tang <= positive)
-			Tang *= 10;
-		tens = Tang / 10;
+		
+		if ((positive / 10) > 0)
+			print_number(positive / 10);
 
-		while (tens >= 1)
-		{
-			digits = positive / tens;
-			_putchar(digits + '0');
-			positive = (positive - (tens * digits));
-				tens / 10;
-		}
-	}
+		_putchar((positive % 10) + 48);
 }
