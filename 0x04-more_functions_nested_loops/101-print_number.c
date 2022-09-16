05d075b713743195e52ebfc0ca6fb3e61675f4c0
Precious Ekene <83302652+preshykene@users.noreply.github.com>
2022-09-16 09:08:50 +0100

    Updated 101 again

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,46 @@
 #include "main.h"
 
 /**
- * print_number - print an integer, without using long, arrays, or pointers
- * @n: number to be printed
+ * print_number - prints number
+ *
+ * @n: integer to print to character
  */
-
 void print_number(int n)
 {
-	unsigned int tens, digit, positive = n;
-	double Tang = 1;
+	int i;
+	int d = 1;
+	unsigned int x = n;
+	unsigned int y = n;
+	int c = 0;
 
 	if (n == 0)
-		_putchar('0');
-	else
 	{
-		if (n < 0)
-		{
-			positive = n * -1;
-			_putchar('-');
-		}
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n + 1;
+		n = -n;
+		y = n;
+		x = n;
+		x += 1;
+		y += 1;
+	}
 
-		while (Tang <= positive)
-			Tang *= 10;
-		tens = Tang / 10;
-
-		while (tens >= 1)
-		{
-			digit = positive / tens;
-			_putchar(digit + '0');
-			positive = (positive - (tens * digit));
-			tens /= 10;
-		}
+	while (x != 0)
+	{
+		x = x / 10;
+		c++;
+	}
+	for (i = 1; i < c; i++)
+	{
+		d *= 10;
+	}
+	for (i = 0; i < c; i++)
+	{
+		_putchar(y / d + '0');
+		y = y % d;
+		d = d / 10;
 	}
 }
