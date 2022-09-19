c70cf6ba148bffd22b6b5c7ecee13b6175115e83
preshykene <preciousekene51@gmail.com>
2022-09-19 05:49:52 -0400

    Done with 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
 * _puts - puts a string
@@ -9,11 +8,8 @@
 
 void _puts(char *str)
 {
-	int i;
+	char i;
 
 	for (i = 0; *(str + i) != '\0'; i++)
-	{
-		_putchar(*(str + i));
-	}
-	_putchar('\n');
+		_puts(str);
 }
