9d55ace46fa08a2a51634a7b4c91d5285f8bb40d
preshykene <preciousekene51@gmail.com>
2022-09-19 05:34:26 -0400

    Done with 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,19 @@
 #include "main.h"
+#include "2-strlen.c"
+
 /**
+* _puts - puts a string
 *
-*
-*
+* @str: string to print
 */
 
 void _puts(char *str)
 {
-	*str = "I do not fear computers. I fear the lack of them - Isaac Asimov"
-	_puts(str);
+	int i;
+	
+	for (i = 0; *(str + i) != '\0' i++)
+	{
+		_putchar(*(str +));
+	}
+	_putchar('\n');
 }
