536f294d989cd2d66e73f9c4aabf1e3ecdcb4c79
preshykene <preciousekene51@gmail.com>
2022-09-19 07:26:24 -0400

    Done with 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,13 @@
 
 /**
  * _puts - prints a string, followed by a newline to stdout
- *
  * @str: string parameter to print
- *
- * Return: Nothing
-*/
+ */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
-		_puts(*str char);
-		++str;
+		_putchar(*str++);
+		
+	_putchar('\n');
 }
