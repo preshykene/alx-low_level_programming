1c22a112ea8ee7518fac04be8c7a56774508f6a0
preshykene <preciousekene51@gmail.com>
2022-09-19 07:44:04 -0400

    Done with 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,23 +4,23 @@
  * print_rev - reverse a string value
  * @s: print string in reverse
  */
- 
+
 void print_rev(char *s)
 {
 	int i;
-	int len =0;
-	
+	int len = 0;
+
 	while (*s != '\0')
 	{
 		len++;
-		s++;
+		++s;
 	}
 
 	for (i = len; i > 0; i--)
 	{
 		_putchar(*s);
-		s--;
+		--s;
 	}
-	
+
 	_putchar('\n');
 }
