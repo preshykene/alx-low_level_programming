679147bbfd8d51ecf45cc28409f636e517703782
preshykene <preciousekene51@gmail.com>
2022-09-19 04:38:46 -0400

    Done with 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - swaps two integer
+*@a: integer assigned to a
+*@b: integer assigned to b
+*
+*/
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	*a = c;
+	*b = d;
+}
