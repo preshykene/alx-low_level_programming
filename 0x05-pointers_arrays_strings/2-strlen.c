60cbedf2de3283fa55a8be846958ec5a0ad7d245
preshykene <preciousekene51@gmail.com>
2022-09-19 05:07:52 -0400

    Done with 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,9 +4,14 @@
 * _strlen - calculates string length
 * @s: assigned character
 *
+* Return: returns the strlen of i
 */
 
 int _strlen(char *s)
 {
-	_strlen(s);
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
 }
