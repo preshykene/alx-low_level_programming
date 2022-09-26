8b0c6ab97f88cc1160c528d02975f3ead12b087d
preshykene <preciousekene51@gmail.com>
2022-09-26 07:54:33 -0700

    Done with 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _strspn - prefix of substring
 * @s: string to go through
@@ -22,9 +23,7 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 		if (s[j] != '\n')
-		{
 			return (length);
-		}
 	}
 	return (length);
 }
