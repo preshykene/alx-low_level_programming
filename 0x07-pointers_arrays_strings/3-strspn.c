3b322c67c7fb127dfbdd9ba787bd6b461cb00a6f
preshykene <preciousekene51@gmail.com>
2022-09-26 07:39:13 -0700

    Done with 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _strspn - prefix of substring
+* @s: string to go through
+* @accept: accepted bytes
+* Return: Returns unsigned values
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int length = 0;
+
+	for (i = 0; s[i] != '\n'; i++)
+	{
+		for (j = 0; s[j] != '\n'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+			length++;
+			break;
+			}
+		}
+		if (s[j] != '\n')
+		{
+			return (length);
+		}
+	}
+	return (length);
+}
