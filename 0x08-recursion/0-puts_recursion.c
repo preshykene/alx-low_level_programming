4af5f3d8f2768f24a212c317ff50ddb44f5a236c
preshykene <preciousekene51@gmail.com>
2022-09-28 05:08:11 -0400

    Done with 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
  * _puts_recursion - prints a string
  * @s: string to print
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(++s);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
