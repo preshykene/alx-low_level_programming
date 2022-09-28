3a29cdf6598389728691ad5f431701f4e9611f07
preshykene <preciousekene51@gmail.com>
2022-09-28 05:51:49 -0400

    Done with 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
-*
-*
-*
+* _pow_recursion - prints the power of a number recursively
+* @x: first parameter
+* @y: second parameter
+* Return: Returns result or -1
 */
 int _pow_recursion(int x, int y)
 {
@@ -12,7 +13,7 @@ int _pow_recursion(int x, int y)
 	}
 	if (y > 0)
 	{
-		return (x * _powrecursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 	return (1);
 }
