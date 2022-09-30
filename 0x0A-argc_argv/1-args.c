6bc836cf046af40085b9b8f735266dcc29f05368
preshykene <preciousekene51@gmail.com>
2022-09-30 04:23:40 -0700

    Done with 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-* @argc: argument count
-* @argv: argument vector
-* Return: always set to 0
+ * main - a program that prints all arguments it receives
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
 */
 
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0, m;
+	int i;
 
-	while (i < argc)
-	{
-		m = i;
-		i++;
-	}
-	printf("%d\n", m);
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-* @argc: argument count
-* @argv: argument vector
-* Return: always return 0
+ * main - a program that prints all arguments it receives
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
-	{
-	printf("%s\n", argv[i]);
-	}
-	printf("\n");
+		printf("%s\n", argv[i]);
 	return (0);
 }
