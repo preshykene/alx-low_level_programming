d52dc5821d98559eaa6917bfc74597cce562c2cb
preshykene <preciousekene51@gmail.com>
2022-10-03 02:23:58 -0700

    Done with 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *t;
 	if (size <= 0)
 		return (NULL);
+	t = malloc(sizeof(char) * size);
 	if (t == NULL)
 		return (NULL);
-	t = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		t[i] = c;
 	return (t);
