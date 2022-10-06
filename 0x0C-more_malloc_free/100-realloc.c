bbfbd770b0c632d1e71b57fdeef88831edd2b967
preshykene <preciousekene51@gmail.com>
2022-10-06 13:05:43 -0700

    Done with 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,40 +1,40 @@
-#include "main.h" 
- #include <stdlib.h> 
-  
- /** 
-  * _realloc - reallocates a memory block using malloc and free. 
-  * @ptr: pointer to previously allocated memory 
-  * @old_size: size of allocated space for ptr 
-  * @new_size: size of newly allocated space 
-  * 
-  * Return: pointer to newly allocated memory, or NULL if failure 
-  */ 
- void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) 
- { 
-         char *p; 
-         unsigned int i, max = new_size; 
-         char *oldp = ptr; 
-  
-         if (ptr == NULL) 
-         { 
-                 p = malloc(new_size); 
-                 return (p); 
-         } 
-         else if (new_size == 0) 
-         { 
-                 free(ptr); 
-                 return (NULL); 
-         } 
-         else if (new_size == old_size) 
-                 return (ptr); 
-  
-         p = malloc(new_size); 
-         if (p == NULL) 
-                 return (NULL); 
-         if (new_size > old_size) 
-                 max = old_size; 
-         for (i = 0; i < max; i++) 
-                 p[i] = oldp[i]; 
-         free(ptr); 
-         return (p); 
+#include <stdlib.h>
+#include "main.h"
+/**
+* _realloc - reallocates a memory block using malloc and free.
+* @ptr: pointer to the memory previously allocated with a
+* call to ``` malloc : malloc(old_size) ```.
+* @old_size: size in bytes allocated for ptr.
+* @new_size: size in bytes of new memory block.
+* Return: pointer to new mem block, NULL or ptr.
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if ((new_size == 0) && (ptr != NULL)
+	{
+                 free(ptr);
+                 return (NULL);
+         }
+         if (ptr == NULL)
+         {
+                 nptr = malloc(new_size);
+                 if (nptr == NULL)
+                         return (NULL);
+         }
+         if (new_size > old_size && (ptr != NULL))
+         {
+                 nptr = malloc(new_size);
+                 if (nptr == NULL)
+                         return (nptr);
+                 for (i = 0; i < old_size; i++)
+                         nptr[i] = *((char *)ptr + 1);
+                 free(ptr);
+         }
+
+         return (nptr);
  }
