bc9888b34b39797b443b6be477b312079b0914b5
preshykene <preciousekene51@gmail.com>
2022-10-10 04:35:52 -0700

    Updated preprocessors again

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,33 +1,22 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
-* print_dog - prints dog variables
-* @d: structure of a dog
+ * print_dog - print dog
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: nothing
 */
+
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-		if (d->age == NULL)
-		{
-			printf("Age: (nil)\n");
-		}
-		else
-			printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-			printf("Owner: %s\n", d->owner);
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
