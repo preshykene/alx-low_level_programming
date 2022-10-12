fc14b6ac80315e7abb6fb9499ff472c899dd994f
preshykene <preciousekene51@gmail.com>
2022-10-12 03:50:59 -0400

    Done with function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+* print_name - prints name
+* @name: name to print
+* @f: function to use
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointer"
+/**
+* array_iterator - iterates through an array
+* @array: array to go through
+* @size: size of array
+* @action: action funtion to take
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+	
+	if (array == NULL || action == NULL)
+		exit(98);
+	for (; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
